5e0dde6db2b155f929854bb1c8ff2baa194d78b5
Lutonhospital <Lutonhospital01@gmail.com>
2025-09-27 16:35:00 +0300

    Add admin to public + update redirects

--- vite.config.ts
+++ vite.config.ts
@@ -38,3 +38,21 @@ export default defineConfig({
     },
   },
 });
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'client/admin',
+          dest: '' // copies to /dist/admin
+        }
+      ]
+    })
+  ]
+})
